d546afced6c80bfcbdb680b9f7a920cb55843798
marcosihe <marcosherrera.ib@gmail.com>
2021-09-22 21:01:54 -0300

    share code on github

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+import express from "express";
+import mongoose from 'mongoose';
+import data from "./data.js";
+import userRouter from "./routers/userRouter.js";
+
+const app = express();
+
+mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/amazone", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
+  product
+    ? res.send(product)
+    : res.status(404).send({ message: "Producto no encontrado" });
+});
+
+app.get("/api/products", (req, res) => {
+  res.send(data.products);
+});
+
+app.use("/api/users", userRouter);
+app.get("/", (req, res) => {
+  res.send("Server is ready");
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Serve at http://localhost/${port}`);
+});
